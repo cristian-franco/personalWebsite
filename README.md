57fabc6351278697784a10c886945989ace39d1d
cristian-franco <cristianfranco.cf@gmail.com>
2023-06-29 21:37:18 -0700

    New website template utilized

--- README.md
+++
@@ -1,4 +0,0 @@
-# personalWebsite
-Personal portfolio website.
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/94738ce9-8715-4864-b524-58f3bdb3b147/deploy-status)](https://app.netlify.com/sites/priceless-goodall-ae4dd1/deploys)
--- src/myscript.js
+++
@@ -1,9 +0,0 @@
-var mymap = L.map('mapid').setView([34.0522, -118.2437], 10);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiY2ZyYW5jbzE1IiwiYSI6ImNra2o3Z3BpNDBhOXcyb29hZGFjZHFucWYifQ.SOwlGWL2jOc7ME64jaO-QA'
-}).addTo(mymap);
