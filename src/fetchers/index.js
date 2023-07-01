f317e9ecb2dbfa85835b009239588402286827c3
cristian-franco <cristianfranco.cf@gmail.com>
2023-07-01 16:14:45 -0700

    altered path in fetchers

--- src/fetchers/index.js
+++ src/fetchers/index.js
@@ -1,47 +1,47 @@
 import { http } from "./http";
 
 const getInformation = async () => {
-  let res = await http.get(`/information.json`);
+  let res = await http.get(`/api/information.json`);
   return res.data;
 };
 
 const getServices = async () => {
-  let res = await http.get(`/services.json`);
+  let res = await http.get(`/api/services.json`);
   return res.data;
 };
 
 const getTechskills = async () => {
-  let res = await http.get(`/techskills.json`);
+  let res = await http.get(`/api/techskills.json`);
   return res.data;
 };
 
 const getLanguageskills = async () => {
-  let res = await http.get(`/languageskills.json`);
+  let res = await http.get(`/api/languageskills.json`);
   return res.data;
 };
 
 const getPortfolioFilters = async () => {
-  let res = await http.get(`/portfoliofilters.json`);
+  let res = await http.get(`/api/portfoliofilters.json`);
   return res.data;
 };
 
 const getPortfolios = async () => {
-  let res = await http.get(`/portfolios.json`);
+  let res = await http.get(`/api/portfolios.json`);
   return res.data;
 };
 
 const getJobExperience = async () => {
-  let res = await http.get(`/jobexperience.json`);
+  let res = await http.get(`/api/jobexperience.json`);
   return res.data;
 };
 
 const getEducationBackground = async () => {
-  let res = await http.get(`/educationbackground.json`);
+  let res = await http.get(`/api/educationbackground.json`);
   return res.data;
 };
 
 const getClientReviews = async () => {
-  let res = await http.get(`/clientsreview.json`);
+  let res = await http.get(`/api/clientsreview.json`);
   return res.data;
 };
 
