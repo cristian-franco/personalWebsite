284543ac037c74ba8602b2e6915342e60cf7a9a1
cristian-franco <cristianfranco.cf@gmail.com>
2023-06-30 22:41:09 -0700

    removed toml and .next

--- .next/static/_DIftX07VcbHkubzSTIYE/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fhomepage2","\u002Fhomepage3","\u002F","\u002Fcategory\u002F[slug]\u002F[page]","\u002Fpostdetails\u002F[slug]","\u002Fposts\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
